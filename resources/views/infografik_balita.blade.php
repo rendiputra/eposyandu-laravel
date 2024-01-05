e7dad0d26d3d6c998de770d764b5b795264fe245
rendiputra <rendiputrapradana@gmail.com>
2024-01-05 23:18:08 +0700

    refactor: fix link a href navbar

--- resources/views/infografik_balita.blade.php
+++ resources/views/infografik_balita.blade.php
@@ -18,7 +18,7 @@
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto pt-2 pt-lg-0">
-            <li class="nav-item"><a class="nav-link" aria-current="page" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ route('index') }}#">Home</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('index') }}#tentang">Tentang E-Posyandu</a></li>
             <li class="nav-item"><a class="nav-link fw-bold active" href="{{ route('infografik_balita') }}">Info Grafik</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('index') }}#tka">Tumbuh Kembang Anak</a></li>
